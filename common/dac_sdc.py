118ea91c9e21fb7baf5e24f041ca9f31e3d6ff3b
Jeff Goeders <jeff.goeders@gmail.com>
2020-01-13 23:55:25 +0000

    Updates to example notebook

--- common/dac_sdc.py
+++ common/dac_sdc.py
@@ -3,56 +3,46 @@ import time
 import xml.dom.minidom
 import pathlib
 
-BATCH_SIZE = 500
 DAC_CONTEST = pathlib.Path('/home/xilinx/jupyter_notebooks/dac_sdc_2020/')
 IMG_DIR = DAC_CONTEST / 'images'
 RESULT_DIR = DAC_CONTEST / 'result'
     
-# Get image name list
-def get_image_names():
-    names_temp = [f for f in IMG_DIR.iterdir() if f.suffix == '.jpg']
-    names_temp.sort(key= lambda x:int(x.stem))
-    return names_temp
-
-
-# Process the images in batches, may help when write to XML
-def get_image_batch():
-    image_list = get_image_names()
-    batches = list()
-    for i in range(0, len(image_list), BATCH_SIZE):
-        batches.append(image_list[i:i+BATCH_SIZE])
-    return batches
-
-
-# Get image paths in batches
-def get_image_path(image_name):
-    return os.path.join(IMG_DIR, image_name)
-
 
 # Return a batch of image dir  when `send` is called
 class Team:
-    def __init__(self, teamname):
+    def __init__(self, teamname, batch_size):
         # self.dac_contest = DAC_CONTEST
         # self.img_dir = IMG_DIR                
         # self.result_path = RESULT_DIR        
         self._result_path = RESULT_DIR / teamname
-        self.team_dir = DAC_CONTEST / teamname        
+        self.team_dir = DAC_CONTEST / teamname      
+        self.batch_size = batch_size  
 
         folder_list = [self.team_dir, self._result_path]
         for folder in folder_list:
             if not folder.is_dir():
                 folder.mkdir()
         
-        self.img_list = get_image_names()
-        self.img_batch = get_image_batch()
+        self.img_list = self.get_image_paths()
         self.batch_count = 0
 
+    def get_image_paths(self):
+        names_temp = [f for f in IMG_DIR.iterdir() if f.suffix == '.jpg']
+        names_temp.sort(key= lambda x:int(x.stem))
+        return names_temp
 
-    def send(self, interval_time, batches):
-        time.sleep(interval_time)
-        tmp = batches[self.batch_count]
+    # Returns list of images paths for next batch of images
+    def get_next_batch(self):
+        start_idx = self.batch_count * self.batch_size
         self.batch_count += 1
-        return tmp
+        end_idx = self.batch_count * self.batch_size
+
+        if start_idx >= len(self.img_list):
+            return None
+        elif end_idx > len(self.img_list):
+            return self.img_list[start_idx:]
+        else:
+            return self.img_list[start_idx:end_idx]
 
     def get_bitstream_path(self):
         return str(self.team_dir / "dac_sdc.bit")
@@ -87,7 +77,7 @@ class Team:
 
             doc.appendChild(root)
             name_e = doc.createElement('filename')
-            name_t = doc.createTextNode(self.img_list[i])
+            name_t = doc.createTextNode(self.img_list[i].name)
             name_e.appendChild(name_t)
             image_e.appendChild(name_e)
 
@@ -101,8 +91,6 @@ class Team:
             image_e.appendChild(size_e)
 
             object_node = doc.createElement('object')
-            node_name = doc.createElement('name')
-            node_name.appendChild(doc.createTextNode("NotCare"))
             node_bnd_box = doc.createElement('bndbox')
             node_bnd_box_xmin = doc.createElement('xmin')
             node_bnd_box_xmin.appendChild(
@@ -121,7 +109,6 @@ class Team:
             node_bnd_box.appendChild(node_bnd_box_ymin)
             node_bnd_box.appendChild(node_bnd_box_ymax)
 
-            object_node.appendChild(node_name)
             object_node.appendChild(node_bnd_box)
             image_e.appendChild(object_node)
 
